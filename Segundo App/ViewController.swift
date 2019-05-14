b654f1752a08780aedd7eacdec4c3e036a883927
mxBJJ <maxmendes0509@gmail.com>
2019-05-14 14:39:18 -0300

    first commit

--- Segundo App/ViewController.swift
+++ Segundo App/ViewController.swift
@@ -10,9 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultado: UILabel!
+    
+    @IBAction func gerarNumero(_ sender: Any) {
+        var numero:Int = Int(arc4random_uniform(11))
+        print(numero)
+        resultado.text = String(numero)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
 
